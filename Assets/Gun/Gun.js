8c554b5dd6443e6963092be986e08ebcfca3822a
hamaokah <patstrodr@yahoo.com>
2015-05-04 22:32:45 +0900

    Shooting towards the camera direction

---
+++ Assets/Gun/Gun.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+
+var bulletPrefab : GameObject; // 弾丸のプレハブ
+var initialVelocity : float; // 弾丸の初速
+
+function Update() {
+	 if (Input.GetButtonDown("Fire1")) {
+		 //弾丸のインスタンス化
+		 var bullet : GameObject =
+		  Instantiate(bulletPrefab, transform.position, transform.rotation);
+		 //前方向への速度ベクトルを設定する
+		 var direction : Vector3 = transform.forward;
+		 bullet.GetComponent.<Rigidbody>().velocity = direction * initialVelocity;
+	 }
+}
+		
