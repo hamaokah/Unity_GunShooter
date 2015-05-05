4c9341a7be6e8de8f2c3b38f97b295f1e69ce9ca
hamaokah <patstrodr@yahoo.com>
2015-05-06 01:04:50 +0900

    Added control to shoot at the mouse-clicked point

--- Assets/Gun/Gun.js
+++ Assets/Gun/Gun.js
@@ -9,8 +9,12 @@ function Update() {
 		 //弾丸のインスタンス化
 		 var bullet : GameObject =
 		  Instantiate(bulletPrefab, transform.position, transform.rotation);
-		 //前方向への速度ベクトルを設定する
-		 var direction : Vector3 = transform.forward;
+		 //クリックした点をワールド座標系に変換
+		 var screenPoint = Input.mousePosition;
+		 screenPoint.z = 10.0;
+		 var worldPoint = GetComponent.<Camera>().ScreenToWorldPoint(screenPoint);
+		 //クリックした点へ向かうベクトルを速度ベクトルとして設定
+		 var direction = (worldPoint - transform.position).normalized;
 		 bullet.GetComponent.<Rigidbody>().velocity = direction * initialVelocity;
 	 }
 }
