2dbbc84f42ddf28de102d7661e9be0374c902605
hamaokah <patstrodr@yahoo.com>
2015-05-06 22:28:21 +0900

    Added Random.Range to Box Generator

--- Assets/Box/BoxGenerator.js
+++ Assets/Box/BoxGenerator.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 
+
 var interval_sec : float; // 箱を生成する間隔
 var redBoxPrefab : GameObject; // 赤箱のプレハブ
 var blueBoxPrefab : GameObject; // 青箱のプレハブ
@@ -16,9 +17,15 @@ function Start() {
 function Update() {
 	timer -= Time.deltaTime;
 	if (timer < 0.0) {
+		// 位置と姿勢にランダム成分を与えつつ、箱のプレハブをインスタンス化する
+		var offsx : float = Random.Range(-8.0, 8.0);
+		var offsz : float = Random.Range(-4.0, 4.0);
+		var position : Vector3 = transform.position + Vector3(offsx, 0, offsz);
+		
 		// いずれかの箱のプレハブをインスタンス化する
 		var prefab : GameObject = nextIsRed ? redBoxPrefab : blueBoxPrefab;
-		Instantiate(prefab, transform.position, transform.rotation);
+		Instantiate(prefab, position, Random.rotation);
+		
 		// 次の箱の予約
 		timer = interval_sec;
 		nextIsRed = !nextIsRed;
